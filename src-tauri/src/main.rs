115a540f293e68c96f613461457c0d8e22cca7cb
EineNachtImMai <titouan.cazaubieilh@gmail.com>
2024-03-23 16:17:56 +0100

    main

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,42 @@
+// Prevents additional console window on Windows in release, DO NOT REMOVE!!
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
+use std::fs::{File, OpenOptions};
+use std::io::{Write, BufReader, BufRead, Error};
+
+// use serde_json::Result;
+
+#[tauri::command]
+fn write(path: &str, contents: &str) -> Result<(), Error> {
+    let mut output = OpenOptions::new().write(true).open(path).unwrap();
+    writeln!(output, "{contents}").expect("oopsie woopsie");
+
+    Ok(())
+}
+
+
+#[tauri::command]
+fn read(path: &str) -> Result<String, Error> {
+    let input = File::open(path)?;
+    let buffered = BufReader::new(input);
+
+    let mut together: String = "".to_owned();
+
+    for line in buffered.lines() {
+        together = together + "\n" + line.expect("sure home this works lol").as_str();
+    }
+    
+    Ok(together)
+}
+
+fn main() {
+    let _ = write("/home/blackstar/Documents/programming/rust/project_ant/learn_tauri/files/something.txt", "something something pineapple idk");
+    println!(
+        "{}",
+        read("/home/blackstar/Documents/programming/rust/project_ant/learn_tauri/files/something.txt")
+            .expect("this code is held together by hot glue and tape")
+    );
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
