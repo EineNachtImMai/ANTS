ea362bc76754bf4807184e94746d9413b7d18d4b
EineNachtImMai <titouan.cazaubieilh@gmail.com>
2024-03-30 00:17:35 +0100

    Multiple lines implemented! might discover a few bugs but for now I'm happy with it. Next step: markdown and LaTeX

--- ui/newparagraph.js
+++ ui/newparagraph.js
@@ -27,6 +27,40 @@ function moveCursorToEnd(paragraph) {
     selection.addRange(range); // Set the new range
 }
 
+function collectContents() {
+  let textZonesCollection = document.getElementsByClassName("textzone");
+
+  let parsedContents = "";
+  
+  for (let i=0; i<textZonesCollection.length; i++) {
+    parsedContents = parsedContents + textZonesCollection[i].innerText + "<|>";
+  }
+
+  return parsedContents;
+}
+
+function contentsToHtml(contents) {
+  let parsedContents = contents.split("<|>");
+
+  paragraphCounter = 1;
+  container.innerHTML = "";
+  if (parsedContents.length > 1) {
+    for (let i=0; i<parsedContents.length-1; i++) {
+      createNewChild(container, parsedContents[i]);
+    }
+  } else {createNewChild(container, parsedContents[0])};
+}
+
+function createNewChild(parent, childcontents) {
+  const newChild = document.createElement("p");
+  newChild.id = "p" + paragraphCounter;
+  newChild.contentEditable = true;
+  newChild.className = "textzone";
+  newChild.innerHTML = childcontents;
+  parent.appendChild(newChild);
+  paragraphCounter++;
+}
+
 document.addEventListener('keydown', function(event) {
     if (event.shiftKey && event.key === 'Enter') {
 
@@ -35,6 +69,8 @@ document.addEventListener('keydown', function(event) {
         newParagraph.id = 'p' + paragraphCounter;
 
         newParagraph.contentEditable = true;
+        
+        newParagraph.className = "textzone";
 
         document.activeElement.after(newParagraph);
 
@@ -64,7 +100,7 @@ document.addEventListener('keydown', function(event) {
     moveCursorToEnd(document.activeElement);
     event.preventDefault();
   }
-  else if (event.ctrlKey && event.key === "s") {
+  else if (event.ctrlKey && event.shiftKey && event.key === "S") {
     save({
       filters: [{
         name: "ANTS file",
@@ -72,14 +108,48 @@ document.addEventListener('keydown', function(event) {
       }]
     })
     .then((response) => {
-        currentSavePath = response;
-        currentlyOpenPath = response;
-        invoke('write', {path: currentSavePath, contents: editor.innerText, append: false});
-        // console.log(editor.innerText, currentSavePath)
+        if (response) {
+          currentSavePath = response;
+          currentlyOpenPath = response;
+          invoke('write', {path: currentSavePath, contents: collectContents(), append: false});
+          // console.log(editor.innerText, currentSavePath)
+        }
       })
     .catch((e) => {
         throw new Error(e)
       });
+  }
+  else if (event.ctrlKey && event.key == "s") {
+    if (currentSavePath) {
+      invoke('write', {path: currentSavePath, contents: collectContents(), append: false})
+    }
+    else {
+     save({
+      filters: [{
+        name: "ANTS file",
+        extensions: ["antmd"]
+      }]
+    })
+    .then((response) => {
+        if (response) {
+          currentSavePath = response;
+          currentlyOpenPath = response;
+          invoke('write', {path: currentSavePath, contents: collectContents(), append: false});
+          // console.log(editor.innerText, currentSavePath)
+        }
+      })
+    .catch((e) => {
+        throw new Error(e)
+      });
+ 
+    }
+  }
+
+  else if (event.ctrlKey && event.key === "d") {
+      // handle debug case
+
+    console.log(collectContents());
+      // console.log(document.getElementsByClassName("textzone")[i].innerText); // debug key
   }
 });
 
@@ -99,9 +169,13 @@ butt.addEventListener('click', () => {
     if (result) {
   
       currentlyOpenPath = result;
+      currentSavePath = result;
     
       invoke('read', {path: currentlyOpenPath})
-        .then((response) => {editor.innerHTML = response});
+        .then((response) => {
+          // editor.innerHTML = response;
+          contentsToHtml(response);
+        });
       // console.log(currentlyOpenPath)
     }
   })
